936decfcf34ccfb684b3e9ee98bb003ef27ff8bf
lsgy <1069793806@qq.com>
2017-07-13 22:30:29 +0800

    Update PostDao.java

--- src/main/java/party/pjc/blog/dao/PostDao.java
+++ src/main/java/party/pjc/blog/dao/PostDao.java
@@ -9,26 +9,26 @@ import party.pjc.blog.model.Post_Tags;
 
 public interface PostDao {
 
-	public Post findPostById(int id);
-	public Post findPostByTitle(String title);
-	public List<Post> findAllPost(int id);
+	public Post findPostById(int id);//根据ID查找博文
+	public Post findPostByTitle(String title);//根据标题查找博文
+	public List<Post> findAllPost(int id);//查找所有博文
 	
-	public int insertPost(Post post);
-	public int insertPostAndTag(Post_Tags post_tag);
-	public int insertPostAndCate(Post_Categories post_cate);
-	public int updatePost(Post post);
-	public int deletePost(int id);
+	public int insertPost(Post post);//插入博文
+	public int insertPostAndTag(Post_Tags post_tag);//插入博文标签
+	public int insertPostAndCate(Post_Categories post_cate);//插入博文类型
+	public int updatePost(Post post);//更新博文
+	public int deletePost(int id);//删除博文
 	public int updatePostByRate(Post post);
 	
-	public List<Post> findPostLimit(PageBean pageBean);
+	public List<Post> findPostLimit(PageBean pageBean);//分页显示博文
 	
-	public int getPostCount();
-	
-	
-	public List<Post> selectPostsAndTags(PageBean pageBean);
+	public int getPostCount();//博文总数
 	
 	
-	public Post findUpPost(Integer id);
-	public Post findDownPost(Integer id);
+	public List<Post> selectPostsAndTags(PageBean pageBean);//分页显示博文和标签
+	
+	
+	public Post findUpPost(Integer id);//返回上一页博文
+	public Post findDownPost(Integer id);//返回下一页博文
 	
 }
